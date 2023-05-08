971c492b8aa2a29e07985c70c4014ef4ec7af0c8
Roberto Melero <132332652+Robermelero@users.noreply.github.com>
2023-05-08 16:29:23 +0200

    Update person.js

--- Poo/person.js
+++ Poo/person.js
@@ -25,12 +25,12 @@ class Person{
     }
 
     printAll(){
-        return `name - ${this.name},${"\n"}surname - ${this.surname},${"\n"}height - ${this.height},${"\n"}weight - ${this.weight},${"\n"}year of birth - ${this.yearOfTheBirth}`;
+        console.log(`name - ${this.name},${"\n"}surname - ${this.surname},${"\n"}height - ${this.height},${"\n"}weight - ${this.weight},${"\n"}year of birth - ${this.yearOfTheBirth}`);
                
     }
 
     printHobbies(){
-        return this.hobbies;
+        console.log(this.hobbies);
     }
 
 
@@ -40,8 +40,8 @@ let person1 = new Person("Paco", "Perez", 1.70, 70);
 person1.yearOfTheBirth = 2004;
 person1.hobbies= ["tennis", "football", "eat"];
 // console.log(person1.calImc());
-// console.log(person1.printAll());
+person1.printAll();
 // console.log(person1.calAge(2023));
-// console.log(person1.printHobbies());
+person1.printHobbies();
 
 module.exports = {Person}
