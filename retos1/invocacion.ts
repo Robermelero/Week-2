23fa9b6b561a7f2f6d0980e682299149e0c190c1
Roberto Melero <132332652+Robermelero@users.noreply.github.com>
2023-05-08 16:56:59 +0200

    Update invocacion.ts

--- retos1/invocacion.ts
+++ retos1/invocacion.ts
@@ -2,13 +2,12 @@ import { add } from "./buclesFor";
 import { functionIsEven } from "./condicionales";
 
 
-let arr1:string[] = ["Casa", "Coche", "Ciudad", "Cesta"];
-let arr2:string[] = ["Barco", "Baca", "Bicicleta", "Balon", "Bisiesto", "Brasil"]
-let arr3:string[] = ["Venezuela", "Veneno", "Voltaje"];
-
-let array4:string[] = [...arr1,...arr2,...arr3]
-
-
-
-let array5 = add(array4)
-console.log(functionIsEven(array5));
+let suma:number = add(["Casa","Coche","Ciudad","Cesta"])
+console.log(suma)
+console.log(isEven(suma))
+let suma2:number = add(["Barco","Baca","Bicicleta","Balon","Bisiesto","Brasil"])
+console.log(suma2)
+console.log(isEven(suma2))
+let suma3:number = add(["Venezuela","Veneno","Voltaje"])
+console.log(suma3)
+console.log(isEven(suma3))
