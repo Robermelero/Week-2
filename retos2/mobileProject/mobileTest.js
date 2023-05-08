3d13af98d78ad658b52dd6fefd2bc3dabbed8a73
Roberto Melero <roberto.meleros@gmail.com>
2023-05-08 09:57:19 +0200

    e

--- retos2/mobileProject/mobileTest.js
+++ retos2/mobileProject/mobileTest.js
@@ -4,6 +4,7 @@ var mobile_1 = require("./mobile");
 var mobile1 = new mobile_1.Mobile("Iphone", "Apple", "Iphone13", "spacial grey", 1200);
 var mobile2 = new mobile_1.Mobile("Note", "Mi", "Readme", "green", 720);
 var mobile3 = new mobile_1.Mobile("Samsung", "Samsung", "s21", "blue", 1050);
+var mobile4 = new mobile_1.Mobile("Nokia", "Nokia", "3300", "white", 299);
 // console.log(mobile1);
 // mobile1.setName("Samsung");
 // mobile1.setTrademark("Samsung");
@@ -15,6 +16,7 @@ var mobile3 = new mobile_1.Mobile("Samsung", "Samsung", "s21", "blue", 1050);
 // console.log(mobile1.getModel());
 // console.log(mobile1.getColor());
 // console.log(mobile1.getPrice());
-var myMobile = [mobile1, mobile2, mobile3];
+var myMobile = [mobile1, mobile2, mobile3, mobile4];
 for (var i = 0; i < myMobile.length; i++)
-    console.log(myMobile[i].toPrint(myMobile));
+    ;
+// console.log (myMobile[i].toPrint(myMobile))
--- retos2/mobileProject/mobileTest.ts
+++ retos2/mobileProject/mobileTest.ts
@@ -3,6 +3,7 @@ import { Mobile } from "./mobile";
 let mobile1:Mobile = new Mobile("Iphone", "Apple", "Iphone13", "spacial grey", 1200 );
 let mobile2:Mobile = new Mobile("Note", "Mi", "Readme", "green", 720);
 let mobile3:Mobile = new Mobile("Samsung", "Samsung", "s21", "blue", 1050);
+let mobile4:Mobile = new Mobile("Nokia", "Nokia", "3300", "white", 299);
 // console.log(mobile1);
 // mobile1.setName("Samsung");
 // mobile1.setTrademark("Samsung");
@@ -14,8 +15,10 @@ let mobile3:Mobile = new Mobile("Samsung", "Samsung", "s21", "blue", 1050);
 // console.log(mobile1.getModel());
 // console.log(mobile1.getColor());
 // console.log(mobile1.getPrice());
-let myMobile = [mobile1, mobile2, mobile3];
-for ( let i = 0; i<myMobile.length; i++)
-console.log (myMobile[i].toPrint(myMobile));
+let myMobile = [mobile1, mobile2, mobile3, mobile4];
+for ( let i = 0; i<myMobile.length; i++);
+
+// console.log (myMobile[i].toPrint(myMobile))
+
 
 
