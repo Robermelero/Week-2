3c35f2dd4b20ec9b17e19f37f0e4bd161a382df4
Roberto Melero <132332652+Robermelero@users.noreply.github.com>
2023-05-08 16:59:47 +0200

    Update invocacion.ts

--- retos1/invocacion.ts
+++ retos1/invocacion.ts
@@ -4,10 +4,10 @@ import { functionIsEven } from "./condicionales";
 
 let suma:number = add(["Casa","Coche","Ciudad","Cesta"])
 console.log(suma)
-console.log(isEven(suma))
+console.log(functionIsEven(suma))
 let suma2:number = add(["Barco","Baca","Bicicleta","Balon","Bisiesto","Brasil"])
 console.log(suma2)
-console.log(isEven(suma2))
+console.log(functionIsEven(suma2))
 let suma3:number = add(["Venezuela","Veneno","Voltaje"])
 console.log(suma3)
-console.log(isEven(suma3))
+console.log(functionIsEven(suma3))
